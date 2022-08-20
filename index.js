7511ecfc65521ef79b7ee059682eb2bd57fad74e
fusion407 <fusion407@gmail.com>
2022-08-20 02:15:11 -0500

    removed backgorund description, added alignment dropdown with description. Ability points input is now textbox instead of dropdown

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const raceDropdown = document.querySelector('select#race')
+const alignmentDropdown = document.querySelector('select#alignment')
 const classDropdown = document.querySelector('select#class')
 const backgroundDropdown = document.querySelector('select#background')
 
 
-
-function changeRaceDescription() {
+// work in progress
+// function getSubRace(race) {
+//     console.log("called subrace description function")
+//     if(race === raceDropdown.value) {
+//         console.log("fetching subrace...")
+//         fetch(`https://www.dnd5eapi.co/api/races/${raceDropdown.value}/subraces`, {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 Accept: 'application/json'
+//             }
+//         }) 
+//         .then(response => response.json())
+//         .then(data => {
+//             a = {data: []}
+//             a.data.map(el => (
+//                 console.log(el[results].name[0])
+//             ))
+//             console.log(data.count)
+//         })
+//     }
+//     else {
+//         return;
+//     }
+// }
+async function changeRaceDescription() {
     console.log("called race description function")
-    fetch(`https://www.dnd5eapi.co/api/races/${raceDropdown.value}`, {
+    fetch(`https://www.dnd5eapi.co/api/races/${raceDropdown.value.toLowerCase()}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
@@ -22,11 +47,10 @@ function changeRaceDescription() {
         raceDescription.innerHTML = `<div>
                                         <h2>${data.name}</h2>
                                         <p>Languages: ${data.language_desc}</p>
-                                        <p>Alignment: ${data.alignment}</p>
                                      </div>`;
+        // getSubRace(data.name);
     });
 }
-
 function changeClassDescription() {
     console.log("called class description function")
     fetch(`https://www.dnd5eapi.co/api/classes/${classDropdown.value}`, {
@@ -46,42 +70,63 @@ function changeClassDescription() {
     });
 }
 
+
+
 // API currently only supports acolyte so background description
-// returns only its own value
-function changeBackgroundDescription() {
-    console.log("called background description function")
-    const backgroundDescription = document.getElementById('background-description');
-    backgroundDescription.innerHTML = `<div>
-                                            <h2>${backgroundDropdown.value}</h2>
-                                            <p>API not available</p>
-                                        </div>`;
+// returns its own value without fetching data
+// function changeBackgroundDescription() {
+//     console.log("called background description function")
+//     const backgroundDescription = document.getElementById('background-description');
+//     backgroundDescription.innerHTML = `<div>
+//                                             <h2>${backgroundDropdown.value}</h2>
+//                                             <p>API not available</p>
+//                                         </div>`;
+//     fetch(`https://www.dnd5eapi.co/api/backgrounds/${backgroundDropdown.value}`, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             Accept: 'application/json'
+//         }
+//     }) 
+//     .then(response => response.json())
+//     .then(data => {
+//         const backgroundDescription = document.getElementById('background-description');
+//         backgroundDescription.innerHTML = data.name;
+//     });
+// }
 
 
 
-    // fetch(`https://www.dnd5eapi.co/api/backgrounds/${backgroundDropdown.value}`, {
-    //     method: 'GET',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         Accept: 'application/json'
-    //     }
-    // }) 
-    // .then(response => response.json())
-    // .then(data => {
-    //     const backgroundDescription = document.getElementById('background-description');
-    //     backgroundDescription.innerHTML = data.name;
-    // });
+function changeAlignmentDescription() {
+    console.log("called class description function")
+    fetch(`https://www.dnd5eapi.co/api/alignments/${alignmentDropdown.value}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        }
+    }) 
+    .then(response => response.json())
+    .then(data => {
+        const alignmentDescription = document.getElementById('alignment-description');
+        alignmentDescription.innerHTML = `<div>
+                                            <h2>${data.name}</h2>
+                                            <p>Alignment: ${data.desc}</p>
+                                          </div>`;
+    });
 }
-
 document.addEventListener("DOMContentLoaded", () => {
     // containers
     const newCharacterContainer = document.getElementsByClassName('new-character-container')
     const createCharacterContainer = document.getElementsByClassName('create-character-container')
+
     // buttons
     const newCharacterBtn = document.getElementById('new-character-button')
+    
     // description boxes
     const classDescription = document.getElementById('class-description')
     const backgroundDescription = document.getElementById('background-description')
-    // drop down select
+
     // hides new character container and displays create character container
     newCharacterBtn.addEventListener('click', () => {
         console.log('i work')
@@ -89,18 +134,21 @@ document.addEventListener("DOMContentLoaded", () => {
         createCharacterContainer[0].style.display = 'flex'
         changeRaceDescription();
         changeClassDescription();
-        changeBackgroundDescription();
+        changeAlignmentDescription();
+        // changeBackgroundDescription();
     })
 
     // dropdown event listeners
     raceDropdown.addEventListener('change', (e) => {
-        e.preventDefault();
         changeRaceDescription();
     })
     classDropdown.addEventListener('change', (e) => {
         changeClassDescription();
     })
-    backgroundDropdown.addEventListener('change', (e) => {
-        changeBackgroundDescription();
+    // backgroundDropdown.addEventListener('change', (e) => {
+    //     changeBackgroundDescription();
+    // })
+    alignmentDropdown.addEventListener('change', (e) => {
+        changeAlignmentDescription();
     })
 })
